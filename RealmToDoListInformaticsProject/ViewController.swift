38b6792a14165f07e489fe368588a763e793123b
Kirill12a <kirilldrozd12@gmail.com>
2021-04-28 08:49:50 +0300

    Initial Commit

---
+++ RealmToDoListInformaticsProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmToDoListInformaticsProject
+//
+//  Created by Kirill Drozdov on 28.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
